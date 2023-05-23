4d3467b60223bd3e68426e217bfa71f862d44915
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2023-05-23 10:42:04 +0700

    update class component

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -8,25 +8,25 @@ const Footer = () => {
     return (
         <footer className='pt-8 bg-primary'>
             <div className='container pb-5 mx-auto'>
-                <div className='items-start space-y-6 text-white md:flex md:justify-between'>
+                <div className='space-y-4 text-white md:space-y-0 md:flex md:justify-between'>
                     <div>
-                        <h1 className='mt-2 text-3xl font-primary'>Phone Support</h1>
-                        <p className='mb-2 text-xs uppercase text-white/60'>24 Hours A Day</p>
-                        <span className='text-2xl font-tertiary'>+0123 456 789</span>
+                        <h1 className='text-3xl font-primary'>Phone Support</h1>
+                        <p className='mb-1 text-xs uppercase md:mb-2 text-white/60'>24 Hours A Day</p>
+                        <span className='text-lg font-tertiary'>+0123 456 789</span>
                     </div>
                     <div>
                         <h1 className='text-3xl font-primary'>Follow Us</h1>
-                        <p className='mb-4 text-xs uppercase text-white/60'>Social Media Channels</p>
-                        <div className='flex items-center pb-1 mt-2 text-md gap-x-3'>
+                        <p className='mb-1 text-xs uppercase md:mb-4 text-white/60'>Social Media Channels</p>
+                        <div className='flex items-center text-md gap-x-3'>
                             <BsFacebook />
                             <FaTiktok />
                             <BsInstagram />
                             <BsTwitter />
                         </div>
                     </div>
-                    <div className='mt-3 md:mt-0'>
+                    <div>
                         <h1 className='text-3xl font-primary'>Our Newsletter</h1>
-                        <p className='mb-4 text-xs uppercase text-white/60'>Sign Up For Special Offer</p>
+                        <p className='mb-1 text-xs uppercase md:mb-4 text-white/60'>Sign Up For Special Offer</p>
                         <div className='flex'>
                             <input type="email" className='w-full px-4 py-2 text-black bg-grey-200 focus:outline-none' placeholder='Email' />
                             <button className='btn btn-primary'>Subscribe</button>
@@ -35,7 +35,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className='border-t-[0.1px] border-white/30'>
-                <div className='container pt-2 pb-4 mx-auto text-white/60'>
+                <div className='container pt-2 pb-4 mx-auto text-sm text-white/60'>
                     Copyright &copy; 2023 - Hotel Esex-Esex. All Right Reserved
                 </div>
             </div>
