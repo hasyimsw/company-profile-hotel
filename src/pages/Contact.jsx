d830facecec58c471651ac6b5713081cb12e9330
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2023-05-31 20:15:14 +0700

    updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         <div className='px-4 py-16 bg-white'>
           <div className="container mx-auto">
             <h1 className='mb-8 font-bold text-center text-7xl'>Durung Enek Halamane Bosss</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam tempora minima facilis iusto est, quasi laudantium assumenda, amet deserunt dolores, necessitatibus laborum pariatur. Molestias impedit suscipit, nesciunt debitis sit, eos quo nostrum quas eveniet ratione ab odit cupiditate iusto reiciendis veniam laboriosam aut minus eius rem vitae ipsam ullam? Non labore autem temporibus odit et! Sed, necessitatibus! Unde cum ullam ipsum modi veniam fugit repudiandae! Facere sit odit ipsum unde nemo dicta, exercitationem doloribus! Odio omnis ratione, labore quos et atque, nulla numquam dolor laboriosam tempora similique soluta ex molestiae iste reiciendis vitae veritatis deserunt. Velit corporis suscipit nemo dolor?</p>
+            <p>Loremmmm ipsum dolor sit amet consectetur adipisicing elit. Veniam tempora minima facilis iusto est, quasi laudantium assumenda, amet deserunt dolores, necessitatibus laborum pariatur. Molestias impedit suscipit, nesciunt debitis sit, eos quo nostrum quas eveniet ratione ab odit cupiditate iusto reiciendis veniam laboriosam aut minus eius rem vitae ipsam ullam? Non labore autem temporibus odit et! Sed, necessitatibus! Unde cum ullam ipsum modi veniam fugit repudiandae! Facere sit odit ipsum unde nemo dicta, exercitationem doloribus! Odio omnis ratione, labore quos et atque, nulla numquam dolor laboriosam tempora similique soluta ex molestiae iste reiciendis vitae veritatis deserunt. Velit corporis suscipit nemo dolor?</p>
           </div>
         </div>
       </section>
