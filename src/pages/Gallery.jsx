14564d004063ca91bc8b1f2c5b170efae020fa6a
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2023-06-02 09:31:46 +0700

    updated

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -14,7 +14,7 @@ const Gallery = () => {
         <ScrollToTop />
         <div className="container h-screen mx-auto">
           <h1 className='pt-24 pb-8 font-bold text-center text-7xl'>Durung Enek Halamane Bosss</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam tempora minima facilis iusto est, quasi laudantium assumenda, amet deserunt dolores, necessitatibus laborum pariatur. Molestias impedit suscipit, nesciunt debitis sit, eos quo nostrum quas eveniet ratione ab odit cupiditate iusto reiciendis veniam laboriosam aut minus eius rem vitae ipsam ullam? Non labore autem temporibus odit et! Sed, necessitatibus! Unde cum ullam ipsum modi veniam fugit repudiandae! Facere sit odit ipsum unde nemo dicta, exercitationem doloribus! Odio omnis ratione, labore quos et atque, nulla numquam dolor laboriosam tempora similique soluta ex molestiae iste reiciendis vitae veritatis deserunt. Velit corporis suscipit nemo dolor?</p>
+          <p>Lorem ipsummm dolor sit amet consectetur adipisicing elit. Veniam tempora minima facilis iusto est, quasi laudantium assumenda, amet deserunt dolores, necessitatibus laborum pariatur. Molestias impedit suscipit, nesciunt debitis sit, eos quo nostrum quas eveniet ratione ab odit cupiditate iusto reiciendis veniam laboriosam aut minus eius rem vitae ipsam ullam? Non labore autem temporibus odit et! Sed, necessitatibus! Unde cum ullam ipsum modi veniam fugit repudiandae! Facere sit odit ipsum unde nemo dicta, exercitationem doloribus! Odio omnis ratione, labore quos et atque, nulla numquam dolor laboriosam tempora similique soluta ex molestiae iste reiciendis vitae veritatis deserunt. Velit corporis suscipit nemo dolor?</p>
         </div>
 
         <div className="flex space-x-4">
